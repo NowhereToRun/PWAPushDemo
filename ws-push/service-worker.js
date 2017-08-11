989238682f8181db6446b5482b78cf9eae415cd0
NowhereToRun <452242153@qq.com>
2017-08-11 15:20:17 +0800

    add websocket

--- ws-push/service-worker.js
+++ ws-push/service-worker.js
@@ -56,7 +56,7 @@ self.addEventListener('install', function (e) {
 
 self.addEventListener('activate', function (e) {
   console.log('[ServiceWorker] Activate');
-  // wsInit();
+  wsInit();
   e.waitUntil(
     caches.keys().then(function (keyList) {
       return Promise.all(keyList.map(function (key) {
