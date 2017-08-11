015690c3a3ad21cad711135800aaa8c591a130d2
NowhereToRun <452242153@qq.com>
2017-08-11 16:28:44 +0800

    update

--- ws-push/js/app.js
+++ ws-push/js/app.js
@@ -13,7 +13,7 @@
 
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
-            .register('./service-worker.js')
+            .register('./sw.js')
             .then(function () { console.log('Service Worker Registered'); });
 
         showNotification();
