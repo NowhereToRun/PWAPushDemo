03f66f251d4bdf4af311196ee28d80da84d07e0b
NowhereToRun <452242153@qq.com>
2017-09-08 10:39:22 +0800

    测试添加主屏

--- empty/js/app.js
+++ empty/js/app.js
@@ -1,18 +1,5 @@
 (function () {
     'use strict';
-
-    const precacheBtn = document.querySelector('.j_precache_btn');
-
-    function bindEvent() {
-        precacheBtn.addEventListener('click', function () {
-            fetch('https://my_fake.api.com').then(function (response) {
-                console.log(response);
-            }).catch(function (e) {
-                console.log("Oops, error");
-            });
-        })
-    }
-
     function showNotification() {
         Notification.requestPermission(function (result) {
             if (result === 'granted') {
@@ -28,9 +15,8 @@
             .register('./service_worker.js')
             .then(function () {
                 console.log('Service Worker Registered');
-                bindEvent();
             });
-        showNotification();
+        // showNotification();
     }
 
 
