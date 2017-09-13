f545587daecea7de09546c6bffdc6a99f998da48
NowhereToRun <452242153@qq.com>
2017-09-13 14:48:32 +0800

    删除无用代码

--- empty/service_worker.js
+++ empty/service_worker.js
@@ -1,19 +1,5 @@
-// Copyright 2016 Google Inc.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//      http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 var dataCacheName = 'precache-next-page-v1';
-var cacheName = 'precache-static-v1';
+var cacheName = 'precache-static-v2';
 var filesToCache = [
   './',
   './index.html',
@@ -50,19 +36,7 @@ self.addEventListener('fetch', function (e) {
   console.log('[Service Worker] Fetch', e.request.url);
   var fakeUrl = 'https://my_fake.api.com';
   if (e.request.url.indexOf(fakeUrl) > -1) {
-    console.log('fake url');
-    var nextPage = new Request('./nextPage/precache');
-    console.log(nextPage);
-    e.waitUntil(
-      fetch(nextPage).then(function (response) {
-        return caches.open(dataCacheName).then(function (cache) {
-          console.log('Cached next page ' + response.url);
-          return cache.put(nextPage, response).then(function () {
-            console.log('cache put success')
-            return new Response({url:'https://my_fake.api.com'});
-          })
-        });
-      }));
+
   } else {
     /*
      * The app is asking for app shell files.
